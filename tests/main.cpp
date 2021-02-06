1927d01c60cf634484740a4ac46d6dbc25320c4f
Jan Starke <Jan.Starke@t-systems.com>
2021-02-06 22:07:44 +0100

    changed version selector for aixlog

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,15 +3,21 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
-#if !(defined(__GNUC__) && defined(__APPLE__))
-#include <aixlog.hpp>
-#endif
+#if defined(__GNUC__) && defined(__APPLE__)
 
 int main(int argc, char * argv[]) {
-#if !(defined(__GNUC__) && defined(__APPLE__))
+  Catch::Session().run( argc, argv );
+}
+
+#else
+
+#include <aixlog.hpp>
+int main(int argc, char * argv[]) {
   AixLog::Log::init<AixLog::SinkCout>(
           AixLog::Severity::trace,
           AixLog::Type::normal);
-#endif
   Catch::Session().run( argc, argv );
 }
+
+#endif
+
